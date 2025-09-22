274e02f621e4e8fab42949fa7bbccbd2580034c1
AWeiBin <177638742+AWeiBin@users.noreply.github.com>
2025-09-23 00:06:18 +0800

    Add help messages for all commands

--- src/main/java/modhero/commands/ExitCommand.java
+++ src/main/java/modhero/commands/ExitCommand.java
@@ -1,6 +1,15 @@
 package modhero.commands;
 
+/**
+ * Terminates the program.
+ */
 public class ExitCommand extends Command {
+
+    public static final String COMMAND_WORD = "exit";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exits the program.\n"
+            + "  Example: " + COMMAND_WORD;
+
     @Override
     public CommandResult execute() {
         return new CommandResult("Goodbye!");
