3bca4df855cfc6d8de58addbd5e33d6463a5b358
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-09-23 14:03:54 +0800

    Added private and public members to Module class

--- src/main/java/modhero/data/modules/Module.java
+++ src/main/java/modhero/data/modules/Module.java
@@ -1,4 +1,20 @@
 package modhero.data.modules;
 
+import java.util.list;
+
 public class Module {
+    private String code;    // e.g. CS2113
+    private String name;    // e.g. Software Engineering
+    private int mc;         // e.g. modular credits
+    private String type;    // e.g. core, elective, etc.
+    private List<String> prerequisites // e.g. ["CS1010", "CS1231"]
+
+    public Module(String code, String name, int mc, String type, List<String> prerequisites) {
+        this.code = code;
+        this.name = name;
+        this.mc = mc;
+        this.type = type;
+        this.prerequisites = prerequisites;
+    }
+
 }
