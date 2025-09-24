b1d1dd8a0b4a8f701dffa4b581740d31db3f022e
AWeiBin <177638742+AWeiBin@users.noreply.github.com>
2025-09-24 17:15:59 +0800

    Add ModuleList class

--- src/main/java/modhero/commands/Command.java
+++ src/main/java/modhero/commands/Command.java
@@ -1,12 +1,18 @@
 package modhero.commands;
 
+import modhero.ModHero;
 import modhero.data.Timetable;
+import modhero.data.modules.ModuleList;
 
 public abstract class Command {
     protected Timetable data;
+    protected ModuleList electiveList;
+    protected ModuleList coreList;
 
-    public void setData(Timetable data) {
+    public void setData(Timetable data, ModuleList electiveList, ModuleList coreList) {
         this.data = data;
+        this.electiveList = electiveList;
+        this.coreList = coreList;
     }
 
     public abstract CommandResult execute();
