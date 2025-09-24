95cf79f2c2d9a3bd3ae87dddc14f874640bf2f65
AWeiBin <177638742+AWeiBin@users.noreply.github.com>
2025-09-24 17:45:40 +0800

    Add Storage class

---
+++ src/main/java/modhero/storage/Storage.java
@@ -0,0 +1,85 @@
+package modhero.storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Loading from and saving to a text file.
+ */
+public class Storage {
+
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Ensures that the directory for the file path exists.
+     * Creates directories if not present.
+     */
+    public void ensureFileDirectoryExist() {
+        new File(filePath).getParentFile().mkdirs();
+    }
+
+    /**
+     * Loads the file contents into a list of strings, each line a list element.
+     *
+     * @return list of lines from file, or null if file not found
+     */
+    public List<String> load() {
+        try {
+            ensureFileDirectoryExist();
+            return readFromFile();
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Reads all lines from the file at filePath.
+     *
+     * @return list of strings read line-by-line from the file
+     * @throws FileNotFoundException when the file does not exist
+     */
+    private List<String> readFromFile() throws FileNotFoundException {
+        File file = new File(filePath);
+        Scanner s = new Scanner(file);
+        List<String> rawTaskList = new ArrayList<>();
+        while (s.hasNext()) {
+            rawTaskList.add(s.nextLine());
+        }
+        return rawTaskList;
+    }
+
+    /**
+     * Saves the given text string to the file, overwriting any existing content.
+     *
+     * @param textToAdd the text content to save
+     */
+    public void save(String textToAdd) {
+        try {
+            ensureFileDirectoryExist();
+            writeToFile(textToAdd);
+        } catch (IOException e) {
+            System.out.println("Failed save file");
+        }
+    }
+
+    /**
+     * Writes the provided text content to the file at filePath.
+     *
+     * @param textToAdd text to write to the file
+     * @throws IOException if an I/O error occurs during writing
+     */
+    private void writeToFile(String textToAdd) throws IOException {
+        FileWriter fileWriter = new FileWriter(filePath);
+        fileWriter.write(textToAdd);
+        fileWriter.close();
+    }
+}
