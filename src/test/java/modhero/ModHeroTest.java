e5bb440d505d30bc3129d81e78cf87b39050a345
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-10-15 00:14:29 +0800

    Added Test structure

---
+++ src/test/java/modhero/ModHeroTest.java
@@ -0,0 +1,12 @@
+package modhero;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ModHeroTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
