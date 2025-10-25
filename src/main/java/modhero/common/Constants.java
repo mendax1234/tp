efdcf8d031806e3c3c466e4137f39ca3a1283a56
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-10-25 20:25:13 +0800

    Updated NUM_TERMS to be 2 instead of 4

--- src/main/java/modhero/common/Constants.java
+++ src/main/java/modhero/common/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
 
-    public static final int NUM_TERMS = 4;
+    public static final int NUM_TERMS = 2;
     public static final int NUM_YEARS = 4;
 
     public static final String MODULES_FILE_PATH = "data/modules.txt";
