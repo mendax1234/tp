69a9e022775128d11505a2e1f668e1922e917333
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-10-25 21:07:23 +0800

    Defined max modules per sem = 5

--- src/main/java/modhero/common/Constants.java
+++ src/main/java/modhero/common/Constants.java
@@ -3,6 +3,8 @@ package modhero.common;
 public class Constants {
 
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final int MAX_MODULES_PER_SEM = 5;
+
 
     public static final int NUM_TERMS = 2;
     public static final int NUM_YEARS = 4;
